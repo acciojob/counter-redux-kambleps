f72cea41aa2e4befec5baef340c4618ce6eb3147
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-13 12:51:07 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
+import { increment,decrement } from "../action/counteraction";
+import { useSelector,useDispatch } from "react-redux";
 import './../styles/App.css';
 
 const App = () => {
+  const counter = useSelector(state=>state.a)
+  const dispatch=useDispatch();
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Counter:{counter}</h1>
+        <button onClick={()=>dispatch(increment(1))}>Increment</button>
+        <button onClick={()=>dispatch(decrement(1))}>Decrement</button>
     </div>
   )
 }
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import counterReducer from './reducer/counterReducer';
+
+
+const store =  createStore(counterReducer); // store the state variable
+
+
+export default store;
+
